3246fb96c39fb35e00264f3d3dd7522b48a2b5f9
HSzkuu <2335585278@qq.com>
2025-06-20 15:45:20 +0800

    addd giscus & blur & hidden

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,6 +2,8 @@
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
+import giscusTalk from 'vitepress-plugin-comment-with-giscus';
+import { useData, useRoute } from 'vitepress';
 import './style.css'
 import { 
   NolebaseEnhancedReadabilitiesMenu, 
@@ -18,6 +20,30 @@ import '@nolebase/vitepress-plugin-highlight-targeted-heading/client/style.css'
 
 export default {
   extends: DefaultTheme,
+  setup() {
+    // Get frontmatter and route
+    const { frontmatter } = useData();
+    const route = useRoute();
+        
+    // giscus配置
+    giscusTalk({
+      repo: 'HSzkuu/HINATA-Docs-VP', //仓库
+      repoId: 'R_kgDONG01mA', //仓库ID
+      category: 'General', // 讨论分类
+      categoryId: 'DIC_kwDONG01mM4CrwEo', //讨论分类ID
+      mapping: 'pathname',
+      inputPosition: 'bottom',
+      lang: 'zh-CN',
+      }, 
+      {
+        frontmatter, route
+      },
+      //默认值为true，表示已启用，此参数可以忽略；
+      //如果为false，则表示未启用
+      //您可以使用“comment:true”序言在页面上单独启用它
+      true
+    );
+  },
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       'nav-bar-content-after': () => h(NolebaseEnhancedReadabilitiesMenu), 
